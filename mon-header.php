2a908b87a9c70f31a6a5f6df240565bf58894a7f
Magolor <83692134+MagolorTheEggGod@users.noreply.github.com>
2024-02-02 13:47:43 +0100

    Add files via upload
    yea desciption ig

---
+++ mon-header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centre d'Adoption</title>
+    <style>
+        body {
+            background-color: cyan;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #fff;
+            padding: 10px;
+            text-align: center;
+        }
+
+        img {
+            max-width: 100%;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <img src="DOGS.png" alt="Logo DOGS">
+        <h1>Elian Bournisien</h1>
+    </header>
